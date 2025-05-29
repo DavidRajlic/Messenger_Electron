8f10bceec677db24706e947a7e03137059348bb2
DavidRajlic <rajlic.david@gmail.com>
2025-05-29 21:44:59 +0200

    change theme functionality and to read from json

---
+++ renderer.js
@@ -0,0 +1,46 @@
+const settingsBtn = document.getElementById('settings-btn');
+const themeLink = document.getElementById('theme-style');
+
+
+settingsBtn.addEventListener('click', () => {
+  window.api.openSettingsWindow();
+});
+
+
+window.api.onThemeChange((theme) => {
+  const href = theme === 'dark' ? 'styles/dark.css' : 'styles/light.css';
+  themeLink.setAttribute('href', href);
+  localStorage.setItem('theme', theme);
+});
+
+
+const saved = localStorage.getItem('theme') || 'light';
+window.api.onThemeChange((theme) => {}); 
+window.api.setTheme(saved); 
+
+
+document.getElementById('loadBtn').addEventListener('click', async () => {
+  const data = await window.electronAPI.openJsonFile();
+  if (data) showContact(JSON.parse(data));
+});
+
+window.electronAPI.onLoadContactFile((path) => {
+  fetch(`file://${path}`)
+    .then(res => res.json())
+    .then(showContact)
+    .catch(err => console.error('Napaka pri nalaganju:', err));
+});
+
+function showContact(contact) {
+  document.getElementById('firstName').textContent = contact.firstName;
+  document.getElementById('lastName').textContent = contact.lastName;
+  document.getElementById('email').textContent = contact.email;
+  document.getElementById('phone').textContent = contact.phone;
+   if (contact.image) {
+    console.log(contact.image)
+    image.src = `images/${contact.image}`;
+  } else {
+    contactImage.src = ''; 
+  }
+}
+
